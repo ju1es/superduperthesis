029eea70397c4e42d4ebaf110591a706eecd2c88
Jules <jules.sulpico@unanimousai.com>
2018-09-01 14:14:27 -0700

    add convnet example

--- src/lib/dataWrangler.py
+++ src/lib/dataWrangler.py
@@ -10,6 +10,16 @@ def fetchPaths(datasetPath):
     return paths
 
 def saveToTraining(config, datapoint, datapointPath):
+
+    # TODO:
+    # + For each column in transform
+    # --> Reshape.
+    #     + np.abs, rescale to (0 to 1)
+    # --> Generate ground truth array
+    #     + np.arange(Nframes) * t
+    #     ---> Where t = hopLength/sampleRate
+    # --> Use associative .txt to check which pitches are on or off.
+
     _, filename = os.path.split(datapointPath)
     destination = config['TRAINING_SET_PATH'] + os.path.splitext(filename)[0] + ".npy"
     print("Saved " + destination + "...")
