d568dc5749d77260e4efd9d3f1b106d14ff4e326
Jules <jules.sulpico@unanimousai.com>
2018-11-24 21:49:28 -0800

    adjust logfilt shallow dims

--- src/configs.py
+++ src/configs.py
@@ -17,7 +17,7 @@ CONFIG = {
     },
     'TRANSFORMS' : {
         'logfilt_shallow' : {
-                'NUM_BANDS' : 12,
+                'NUM_BANDS' : 48,
                 'NUM_CHANNELS' : 1,
                 'SR' : 44100,
                 'UNIQUE_FILTERS' : True,
--- src/lib/data_wrangler.py
+++ src/lib/data_wrangler.py
@@ -107,7 +107,7 @@ def load_logfilt_mm(data_dir, type, ID):
 
 def load_logfilt_shallow_mm(data_dir, type, ID):
     NOTE_RANGE = 88
-    N_BINS = 78
+    N_BINS = 229
     # 48 = nb, 229
     # 36 = nb, 185
     # 24 = nb, 136
